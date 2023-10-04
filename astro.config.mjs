13034bae4050efef7bdbd0efc006bd41afce0c51
wjh3su <billyjameshowell@gmail.com>
2023-10-04 15:39:58 +0000

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  site: "https://solid-fortnight-4jp456px6693j96j-3000.app.github.dev",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
